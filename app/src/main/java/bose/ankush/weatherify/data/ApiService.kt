9029d7f201612b7fb6e74697923fa8873c273504
ANKUBOSE <ankush.bose@capgemini.com>
2021-09-03 08:09:17 +0530

    Removed click listener

--- app/src/main/java/bose/ankush/weatherify/data/ApiService.kt
+++ app/src/main/java/bose/ankush/weatherify/data/ApiService.kt
@@ -28,43 +28,4 @@ interface ApiService {
         @Query("q") location: String = "Bengaluru",
         @Query("APPID") AppId: String = "ad102b2242e9f1c84075385ae4a91116"
     ): WeatherForecast?
-
-
-    /*companion object {
-        // certificate pinning
-        private val certificatePinner = CertificatePinner.Builder()
-            .add(BASE_URL, "092343KANFJDNSFJN498NDJFNF")
-            .build()
-
-        private val okHttpInterceptor =
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-
-        private val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor(okHttpInterceptor)
-            .addNetworkInterceptor(CustomInterceptor())
-            .certificatePinner(certificatePinner)
-            .connectTimeout(60, TimeUnit.SECONDS)
-            .build()
-
-        fun create(): ApiService = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
-            .build()
-            .create(ApiService::class.java)
-    }*/
 }
-
-/*
-class CustomInterceptor : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val response = chain.proceed(chain.request())
-        val cacheControl = CacheControl.Builder()
-            .maxAge(10, TimeUnit.DAYS)
-            .build()
-        return response.newBuilder()
-            .header("Cache-Control", cacheControl.toString())
-            .build()
-    }
-
-}*/
