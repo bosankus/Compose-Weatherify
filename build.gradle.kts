2831580a2dddd508a2f70b1502ee15b683c9cf85
bosankus <ankush@androidplay.in>
2022-09-08 10:14:22 +0530

    Fixed hilt and benManes plugin - added spotless & ktlint plugins - in progress installations

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,6 @@
 buildscript {
     dependencies {
         classpath(BuildPlugins.buildGradle)
-        classpath(BuildPlugins.hiltPlugin)
-        classpath(BuildPlugins.gradleVersionPlugin)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -12,5 +10,9 @@ plugins {
     id("com.android.application") version Versions.buildGradle apply false
     id("com.android.library") version Versions.buildGradle apply false
     id("org.jetbrains.kotlin.android") version Versions.kotlin apply false
+    id("com.google.dagger.hilt.android") version Versions.hilt apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version Versions.secretPlugin apply false
+    id("org.jlleitschuh.gradle.ktlint") version Versions.ktLintVersion apply false
+    id("com.diffplug.spotless") version Versions.spotlessVersion apply false
+    id("com.github.ben-manes.versions") version Versions.benManes
 }
