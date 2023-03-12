6b1daeed627a1bf19a9389caa1580ad6ea6257c6
Ankush Bose <ankush@androidplay.in>
2023-03-12 13:21:03 +0530

    Added new object for air quality screen - with arguments as pair of lat, lan

--- app/src/main/java/bose/ankush/weatherify/navigation/Screen.kt
+++ app/src/main/java/bose/ankush/weatherify/navigation/Screen.kt
@@ -3,12 +3,22 @@ package bose.ankush.weatherify.navigation
 sealed class Screen(val route: String) {
     object HomeScreen : Screen("home_screen")
     object CitiesListScreen : Screen("city_list_screen")
+    object AirQualityDetailsScreen : Screen("aiq_quality_details_screen")
 
     fun withArgs(vararg args: String?): String {
         return buildString {
             append(route)
             args.forEach { arg ->
                 append("/$arg")
+            }
+        }
+    }
+
+    fun withArgs(vararg args: Pair<String?, String?>): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/lat=${arg.first}/lan=${arg.second}")
             }
         }
     }
