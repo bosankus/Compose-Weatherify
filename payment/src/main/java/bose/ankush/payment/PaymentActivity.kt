bf0ec6a79547378f523cd6da7c0d16806bd3216c
Ankush Bose <ankush@androidplay.in>
2023-07-09 21:38:18 +0530

    Payment: replaced xml with compose - xml payment activity with PaymentScreen.kt

--- payment/src/main/java/bose/ankush/payment/PaymentActivity.kt
+++
@@ -1,8 +0,0 @@
-package bose.ankush.payment
-
-import androidx.fragment.app.Fragment
-
-class PaymentActivity: Fragment() {
-
-
-}
---
+++ payment/src/main/java/bose/ankush/payment/PaymentScreen.kt
@@ -0,0 +1,66 @@
+package bose.ankush.payment
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun PaymentScreen() {
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 128.dp,
+        sheetContent = {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(128.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Swipe up to expand sheet")
+            }
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(64.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Sheet content")
+                Spacer(Modifier.height(20.dp))
+                Button(
+                    onClick = {
+                        scope.launch { scaffoldState.bottomSheetState.partialExpand() }
+                    }
+                ) {
+                    Text("Click to collapse sheet")
+                }
+            }
+        }) { innerPadding ->
+        Box(Modifier.padding(innerPadding)) {
+            Text("Scaffold Content")
+        }
+    }
+}
+
+/*@Composable
+private fun BottomSheetUI() {
+
+}*/
