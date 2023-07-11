4d1c3c0d1ae16b1b9f34459e44d604cad530d6f0
Ankush Bose <ankush@androidplay.in>
2023-07-12 03:21:06 +0530

    AirQualityIndexAnalyser: Added method for prettifying AQI

--- app/src/main/java/bose/ankush/weatherify/presentation/air_quality/AirQualityIndexAnalyser.kt
+++ app/src/main/java/bose/ankush/weatherify/presentation/air_quality/AirQualityIndexAnalyser.kt
@@ -7,15 +7,22 @@ object AirQualityIndexAnalyser {
      * and generate a string accordingly for UI to show
      */
     internal fun getAQIAnalysedText(aqi: Int): Pair<String, Int> {
-        return when(aqi) {
-            in 0..50-> Pair("Air quality is Good at this moment", aqi)
-            in 51..100 -> Pair("Air quality is Moderate at this moment", aqi)
-            in 101..150 -> Pair("Air quality is Unhealthy for sensitive group at this moment", aqi)
-            in 151..200 -> Pair("Air quality is Unhealthy at this moment", aqi)
-            in 201..300 -> Pair("Air quality is Very Unhealthy at this moment", aqi)
-            else -> Pair("Air quality is Hazardous at this moment", aqi)
+        return when (aqi) {
+            in 0..50 -> Pair("Air quality is Good at this moment.", aqi)
+            in 51..100 -> Pair("Air quality is Moderate at this moment.", aqi)
+            in 101..150 -> Pair("Air quality is Unhealthy for sensitive group at this moment.", aqi)
+            in 151..200 -> Pair("Air quality is Unhealthy at this moment.", aqi)
+            in 201..300 -> Pair("Air quality is Very Unhealthy at this moment.", aqi)
+            else -> Pair("Air quality is Hazardous at this moment.", aqi)
         }
     }
 
-
+    /**
+     * This method is actually for making look pretty by adding
+     * adding `0` to single digit number
+     */
+    internal fun Int.getFormattedAQI(): String {
+        return if (this in 0..9) "0$this"
+        else "$this"
+    }
 }
