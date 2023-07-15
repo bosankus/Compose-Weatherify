2df12fb57d1c5e3756150bfb97c2ea1f2c8a8b98
Ankush Bose <ankush@androidplay.in>
2023-07-15 12:19:54 +0530

    Added RunScreen

---
+++ app/src/main/java/bose/ankush/weatherify/presentation/run/RunScreen.kt
@@ -0,0 +1,9 @@
+package bose.ankush.weatherify.presentation.run
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun RunScreen(navController: NavController) {
+
+}
