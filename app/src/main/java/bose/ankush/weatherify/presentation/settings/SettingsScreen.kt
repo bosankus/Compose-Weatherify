1c62e2a82aaeb54d98a97c84f2c2221c5fb662a3
Ankush Bose <ankush@androidplay.in>
2023-07-15 12:20:30 +0530

    Added SettingsScreen

---
+++ app/src/main/java/bose/ankush/weatherify/presentation/settings/SettingsScreen.kt
@@ -0,0 +1,121 @@
+package bose.ankush.weatherify.presentation.settings
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import bose.ankush.weatherify.R
+import bose.ankush.weatherify.base.LocaleConfigMapper
+import bose.ankush.weatherify.common.component.ScreenTopAppBar
+
+@Composable
+fun SettingsScreen(
+    onNavAction: () -> Unit,
+    onLanguageNavAction:(Array<String>) -> Unit
+) {
+
+    val notificationCheckedState = remember { mutableStateOf(false) }
+    val icon: (@Composable () -> Unit)? = if (notificationCheckedState.value) {
+        {
+            Icon(
+                imageVector = Icons.Filled.Check,
+                contentDescription = null,
+            )
+        }
+    } else {
+        null
+    }
+    val languageList = LocaleConfigMapper.getAvailableLanguagesFromJson(
+        jsonFile = "countryConfig.json",
+        context = LocalContext.current
+    )
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            ScreenTopAppBar(
+                headlineId = R.string.app_name, // To be fixed
+                navIconAction = onNavAction
+            )
+        },
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Notification",
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onBackground,
+                    )
+                    Switch(
+                        checked = notificationCheckedState.value,
+                        onCheckedChange = { notificationCheckedState.value = it },
+                        thumbContent = icon
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                        .clickable { onLanguageNavAction.invoke(languageList) },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Language",
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onBackground,
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowRight,
+                        contentDescription = null
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp, start = 16.dp, end = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Get Premium",
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onBackground,
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowRight,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    )
+}
