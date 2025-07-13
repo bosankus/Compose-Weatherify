b99766d6704a306808ab42533e030d68b04679cc
bosankus <ankushbose5@gmail.com>
2025-07-13 20:44:47 +0530

    Migration: rename network weather repository provider and update ProGuard rules

--- app/src/main/java/bose/ankush/weatherify/di/NetworkModule.kt
+++ app/src/main/java/bose/ankush/weatherify/di/NetworkModule.kt
@@ -35,7 +35,7 @@ object NetworkModule {
      */
     @Provides
     @Singleton
-    fun provideNetworkWeatherRepository(
+    fun provideWeatherRepository(
         networkConnectivity: NetworkConnectivity
     ): WeatherRepository {
         return createWeatherRepository(networkConnectivity)
